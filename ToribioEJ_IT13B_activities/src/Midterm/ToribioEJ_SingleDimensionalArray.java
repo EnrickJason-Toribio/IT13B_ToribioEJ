e64e3fe26e54624d10d6203a459c19810e75ce58
Toribio-EnrickJason <toribio.enrickjason@gmail.com>
2025-03-24 11:07:57 +0800

    Added Single Dimensional Array

---
+++ ToribioEJ_IT13B_activities/src/Midterm/ToribioEJ_SingleDimensionalArray.java
@@ -0,0 +1,14 @@
+package Midterm;
+
+public class ToribioEJ_SingleDimensionalArray {
+
+    public static void main(String[] args) {
+
+        char letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+
+        for (char x = 0; x < letters.length; x++) {
+            System.out.print(letters[x] + ", ");
+        }
+    
+}
+}
